35b671b86b5a39233b0bedd3d3fdef4f4c63d1d0
Jarocha75 <jarocha75@gmail.com>
2026-01-18 14:55:26 +0100

    Create the AccountSettingsSkeleton, ProfileSettingsSkeleton

--- src/hooks/useBillings.ts
+++ src/hooks/useBillings.ts
@@ -1,3 +1,4 @@
+import { useAuth } from "@/context/useAuth";
 import type { Billing } from "@/data/billingData";
 import APIClient from "@/services/apiClient";
 import { useQuery } from "@tanstack/react-query";
@@ -5,10 +6,13 @@ import { useQuery } from "@tanstack/react-query";
 const apiClient = new APIClient<Billing>("/billings");
 
 const useBillings = () => {
+  const { isAuthenticated } = useAuth();
+
   return useQuery({
     queryKey: ["billings"],
     queryFn: () => apiClient.getAll(),
     staleTime: 24 * 60 * 60 * 1000,
+    enabled: isAuthenticated,
   });
 };
 
